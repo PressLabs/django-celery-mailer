605f79f5ad50115fd0a2d417913f7a5fbf39a2fb
Manu <Manu>
2014-07-01 14:49:03 +0300

    Remove empty lines.

--- celery_mailer/tasks.py
+++ celery_mailer/tasks.py
@@ -4,7 +4,6 @@ from celery.task import task
 
 from celery_mailer.serializer import deserialize
 
-
 CONFIG = getattr(settings, 'CELERY_EMAIL_TASK_CONFIG', {})
 BACKEND = getattr(settings, 'CELERY_EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
 
@@ -18,21 +17,17 @@ TASK_CONFIG.update(CONFIG)
 
 @task(**TASK_CONFIG)
 def send_email(message, **kwargs):
-
     serializer_type = getattr(settings, 'CELERY_TASK_SERIALIZER', None)
     if serializer_type == 'json':
         message = deserialize(message)
 
     logger = send_email.get_logger()
     conn = get_connection(backend=BACKEND, **kwargs.pop('_backend_init_kwargs', {}))
-
     try:
         result = conn.send_messages([message])
         logger.debug('Successfully sent email message to %r.' % message.to)
         return result
-
     except Exception, e:
-
         # Catching all exceptions b/c it could be any number of things
         # depending on the backend
         logger.warning('Failed to send email message to %r, retrying.' % message.to)
